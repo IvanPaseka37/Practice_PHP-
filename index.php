b10d419739a619de64cebc477018946c4aa45844
IvanPaseka37 <57038294+IvanPaseka37@users.noreply.github.com>
2022-11-16 18:08:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+<?php 
+ $p = 'Bonjour à tous';
+?>
+
+<?php 
+ $name = 'Иван';
+ $surname = 'Пасека';
+ $city = 'Нижний Новгород';
+ $age = 21;
+?>
+
+
+<?php
+include 'main.php';
+?>
---
+++ logo.inc.php
@@ -0,0 +1,4 @@
+    
+                <div class="logo"> 
+                <img src="l.png" alt="php.jfif">
+                </div>                                                 
